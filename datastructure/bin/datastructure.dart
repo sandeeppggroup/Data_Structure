2305b255148c1146422a50a53ec7abf038b0d1ac
Sandeep Abraham <sandeep.pggroup@gmail.com>
2023-06-01 16:17:18 +0530

    initial commit

---
+++ datastructure/bin/datastructure.dart
@@ -0,0 +1,5 @@
+import 'package:datastructure/datastructure.dart' as datastructure;
+
+void main(List<String> arguments) {
+  print('Hello world: ${datastructure.calculate()}!');
+}
---
+++ datastructure/test/datastructure_test.dart
@@ -0,0 +1,8 @@
+import 'package:datastructure/datastructure.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
