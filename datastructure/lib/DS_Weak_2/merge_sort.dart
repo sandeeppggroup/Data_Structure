861bf90fb898dc6d60404cfe5575f78f375658b0
Sandeep Abraham <sandeep.pggroup@gmail.com>
2023-06-27 15:53:25 +0530

    merge Sort

--- datastructure/lib/DS_Weak_2/merge_sort.dart
+++ datastructure/lib/DS_Weak_2/merge_sort.dart
@@ -1,5 +1,53 @@
+// void main() {
+//   List<int> array = [7, 5, 9, 8, 22, 1, 4, 0, 66, 3, 2, 3];
+//   mergeSort(array, 0, array.length - 1);
+//   print(array);
+// }
+
+// void mergeSort(List<int> array1, lb, ub) {
+//   if (lb < ub) {
+//     int mid = (lb + ub) ~/ 2;
+//     mergeSort(array1, lb, mid);
+//     mergeSort(array1, mid + 1, ub);
+//     merge(array1, lb, mid, ub);
+//   }
+// }
+
+// void merge(List<int> array1, int lb, int mid, int ub) {
+//   int i = lb;
+//   int j = mid+1;
+//   int k = lb;
+
+//   List<int> array2 = List<int>.filled(array1.length, 0);
+
+//   while (i <= mid && j <= ub) {
+//     if (array1[i] < array1[j]) {
+//       array2[k] = array1[i];
+//       i++;
+//     } else {
+//       array2[k] = array1[j];
+//       j++;
+//     }
+//     k++;
+//   }
+
+//   while (i <= mid) {
+//     array2[k] = array1[i];
+//     i++;
+//     k++;
+//   }
+//   while (j <= ub) {
+//     array2[k] = array1[j];
+//     j++;
+//     k++;
+//   }
+//   for (int i = lb; i <= ub; i++) {
+//     array1[i] = array2[i];
+//   }
+// }
+
 void main() {
-  List<int> array = [8, 6, 3, 2, 5, 0, 2, 11, 4];
+  List<int> array = [4, 3, 6, 77, 8, 2, 1, 77, 9944, 3];
   mergeSort(array, 0, array.length - 1);
   print(array);
 }
@@ -13,7 +61,7 @@ void mergeSort(List<int> array1, int lb, int ub) {
   }
 }
 
-merge(List<int> array1, int lb, int mid, int ub) {
+void merge(List<int> array1, int lb, int mid, int ub) {
   int i = lb;
   int j = mid + 1;
   int k = lb;
